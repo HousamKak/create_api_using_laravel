c0e0709ce7edfd01713c658f64eaf768890c144e
“Housam <housam.kak20@gmail.com>
2022-09-30 20:06:00 +0300

    Backend => Sorting api: Fixing the variable name succeeded

--- app/Http/Controllers/RandomApiController.php
+++ app/Http/Controllers/RandomApiController.php
@@ -79,7 +79,7 @@ class RandomApiController extends Controller
         // Change the letters into numbers 
         $letter_numerics = [];
         for ($i = 0; $i < count($letter_array); $i++) {
-            array_push($letter_numerics, lettersToNums($$reindexed_numeric_array[$i]));
+            array_push($letter_numerics, lettersToNums($letter_array[$i]));
         };
 
         // Sort the transformed array
@@ -95,6 +95,10 @@ class RandomApiController extends Controller
         for ($i = 0; $i < count($letter_numerics); $i++) {
             array_push($sorted_letters, numsToLetters($reindexed_letter_numerics[$i]));
         };
+
+        // Combine into one string
+        $sorted_string = '';
+
 
         // lower are from 97 to 122, upper are from 65 to 90
         // idea is map 97->1 98->3 i.e. to the odds, and 65->2 66->4 i.e to the evens
@@ -119,8 +123,6 @@ class RandomApiController extends Controller
                 return chr(($num - 1) / 2 + 97);
             }
         }
-
-
 
         return response()->json([
             "status" => "Success",
