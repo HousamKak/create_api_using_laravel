ae66a8f62491cde271c9e8c3791939c2b9fd90ae
“Housam <housam.kak20@gmail.com>
2022-09-30 22:48:34 +0300

    Backend => Routing: Cleaning the routing and adding a version

--- routes/api.php
+++ routes/api.php
@@ -4,34 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RandomApiController;
 
-    //  Workshop material: 
-
-    // Route::group(['prefix' => "v1"], function(){
-
-    //     Route::group(['prefix' => "test"], function(){
-
-    //         Route::group(['middleware' => "role.admin"], function(){
-    //             Route::get("/users", [TestController::class, 'getUsers']);
-    //         });
-
-    //         Route::get("/hi/{age}/{name?}", [TestController::class, 'sayHi']);
-    //         Route::post("/add_user", [TestController::class, 'addUser']);
-
-    //     });
-
-    //     Route::group(['prefix' => "client"], function(){
-
-    //         Route::group(['middleware' => "role.admin"], function(){
-    //             Route::get("/users", [TestController::class, 'getUsers']);
-    //         });
-
-    //         Route::get("/hi", [TestController::class, 'sayHi']);
-    //     })6
-;
-// });
-Route::get("/sorting/{string?}", [RandomApiController::class, 'sortString']);
-Route::get("/numplace/{num?}", [RandomApiController::class, 'numExplode']);
-Route::get("/numtobin/{string?}", [RandomApiController::class, 'numToBinary']);
+Route::group(['prefix' => "v1"], function () {
+    Route::get("/sorting/{string?}", [RandomApiController::class, 'sortString']);
+    Route::get("/numplace/{num?}", [RandomApiController::class, 'numExplode']);
+    Route::get("/numtobin/{string?}", [RandomApiController::class, 'numToBinary']);
+    Route::get("/prefixnotation/{string?}", [RandomApiController::class, 'evalPrefix']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
