1241c265c9dd1fe2b0ddd1329de14598a2ee6e17
“Housam <housam.kak20@gmail.com>
2022-09-30 14:47:33 +0300

    Backend => Laravel: Getting used

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RandomApiController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//  Workshop material: 
+
+// Route::group(['prefix' => "v1"], function(){
+
+//     Route::group(['prefix' => "test"], function(){
+
+//         Route::group(['middleware' => "role.admin"], function(){
+//             Route::get("/users", [TestController::class, 'getUsers']);
+//         });
+
+//         Route::get("/hi/{age}/{name?}", [TestController::class, 'sayHi']);
+//         Route::post("/add_user", [TestController::class, 'addUser']);
+
+//     });
+
+//     Route::group(['prefix' => "client"], function(){
+        
+//         Route::group(['middleware' => "role.admin"], function(){
+//             Route::get("/users", [TestController::class, 'getUsers']);
+//         });
+
+//         Route::get("/hi", [TestController::class, 'sayHi']);
+//     });
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
